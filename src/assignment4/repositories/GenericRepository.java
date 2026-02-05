aeb89267c93afed4dcb56c5e4f59eba082ac2670
didimo777 <momenzanovadilnaz@gmail.com>
2026-02-05 12:11:33 +0500

    Sultan-add reservation payment entities, repository, service

---
+++ src/assignment4/repositories/GenericRepository.java
@@ -0,0 +1,11 @@
+package assignment4.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericRepository<T, ID> {
+    T save(T entity);
+    Optional<T> findById(ID id);
+    List<T> findAll();
+    void deleteById(ID id);
+}
